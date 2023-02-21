9f5b312c821df1911a6f87a21fb01627e9c267b8
Ethew <118255407+JackyTuneur@users.noreply.github.com>
2023-02-21 20:07:28 +0100

    Update tile upload with fs
    Image can now be uploaded locally !

--- src/addFile.js
+++ src/addFile.js
@@ -1,12 +1,13 @@
-const fs = require('fs')
-const path = require("path");
+const fs = require('fs');
+const objectid = require('objectid')
 
 const inputFile = document.getElementById("tileUpload")
 const switchElement = document.getElementById("switchElem")
 
 inputFile.addEventListener("change", (file) => {
-  console.log(inputFile.files[0].path)
+  uploadFile(inputFile.files[0].path)
 })
+
 
 switchElement.addEventListener("click",() => {
   console.log("clicked")
@@ -14,6 +15,23 @@ switchElement.addEventListener("click",() => {
   document.getElementById("windowUpload").classList.toggle("isActive");
 })
 
-const uploadFile = () => {
+const uploadFile = (pathFile) => {
+  console.log(pathFile)
+  let nameOfFile = objectid();
+  let newPath = ("./assets/tiles/temp/" + nameOfFile + ".png")
+  let testReadStream = fs.createReadStream(pathFile)
+  let newFile = fs.createWriteStream(newPath)
+  /*fs.writeFile(pathFile, chunk, (err) => {
+    if(err) throw err
+    console.log("start to do")
+  })*/
+  let chunks = 0;
+  testReadStream.on('data', (chunk) => {
+    chunks += chunk.length;
+  })
+  testReadStream.on('close', () => {
+    console.log("it's uploaded")
+  })
 
+  testReadStream.pipe(newFile)
 }
